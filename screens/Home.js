c933767f9a4f0758c986c05ad9f2452fc0f2cff7
Kevin Vicencio <kevinarnoldcode@gmail.com>
2022-08-24 11:41:04 -0700

    fixing merge

--- screens/Home.js
+++ screens/Home.js
@@ -9,18 +9,6 @@ const Home = () => {
     <SafeAreaView style={{ flex:1 }}>
         <FocusedStatusBar background={COLORS.primary}/>
 
-<<<<<<< HEAD
-        <View style={{ flex:1 }}>
-          <View style={{ zIndex: 0}}>
-            <FlatList 
-              data={NFTData}
-              renderItem={({ item }) => <Text>{item.name}</Text>}
-              keyExtractor = {(item) => item.id}
-              showsVerticalScrollIndicator = {false}
-              ListHeaderComponent={<HomeHeader />}
-            />
-          </View>
-=======
         <View style={{ flex : 1 }}>
             <View style={{ zIndex:0 }}>
                 <FlatList
@@ -31,7 +19,6 @@ const Home = () => {
                     ListHeaderComponent = {<HomeHeader />}
                 />
             </View>
->>>>>>> a0d93dbdf2aeff954629a12dcb7cbc2058d44c6a
         </View>
     </SafeAreaView>
   )
